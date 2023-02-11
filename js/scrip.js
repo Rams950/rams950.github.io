57577d995a0f0a2270c6f0e8c1bca09d210d61db
ramilton Monteiro <rconceicao@uta.cv>
2023-02-11 20:14:49 -0100

    svg-on-html

--- js/scrip.js
+++ js/scrip.js
@@ -46,7 +46,7 @@ let calcScrollValue = () => {
   scrollProgress.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
   });
-  scrollProgress.style.background = `conic-gradient(#00ffff ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  scrollProgress.style.background = `conic-gradient( var(--cor-principal) ${scrollValue}%, var(--cor-secundaria) ${scrollValue}%)`;
 };
 
 window.onscroll = calcScrollValue;
